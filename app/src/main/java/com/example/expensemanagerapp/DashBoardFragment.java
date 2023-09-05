1433d631d983b516c48e6a8eb650b127ba3f92e0
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-09-04 23:15:36 -0300

    Part14 - Floating Button Animation

--- app/src/main/java/com/example/expensemanagerapp/DashBoardFragment.java
+++ app/src/main/java/com/example/expensemanagerapp/DashBoardFragment.java
@@ -7,35 +7,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DashBoardFragment#newInstance} factory method to
+ * Use the {@link DashBoardFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class DashBoardFragment extends Fragment {
+    /*
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_PARAM1 = "param1";
+        private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        // TODO: Rename and change types of parameters
+        private String mParam1;
+        private String mParam2;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        public DashBoardFragment() {
+            // Required empty public constructor
+        }
 
-    public DashBoardFragment() {
-        // Required empty public constructor
-    }
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DashBoardFragment.
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashBoardFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static DashBoardFragment newInstance(String param1, String param2) {
         DashBoardFragment fragment = new DashBoardFragment();
@@ -54,11 +59,76 @@ public class DashBoardFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+*/
+
+    //Floating button
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
+
+    // Floating button textview..
+    private TextView fab_income_txt;
+    private TextView fab_expense_txt;
+
+    //boolen
+    private boolean isOpen=false;
+
+    //Animation.
+    private Animation FadeOpen, FadeClose;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+        View myviem= inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        //Connect floating button to layout
+        fab_main_btn=myviem.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn=myviem.findViewById(R.id.income_ft_btn);
+        fab_expense_btn=myviem.findViewById(R.id.expense_ft_btn);
+
+        // Connect floating text
+        fab_income_txt=myviem.findViewById(R.id.income_ft_text);
+        fab_expense_txt=myviem.findViewById(R.id.expense_ft_text);
+
+
+        //Animation connect...
+        FadeOpen= AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
+        FadeClose=AnimationUtils.loadAnimation(getActivity(), R.anim.fade_close);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isOpen){
+
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_expense_btn.startAnimation(FadeClose);
+                    fab_income_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+
+                    fab_income_txt.startAnimation(FadeClose);
+                    fab_expense_txt.startAnimation(FadeClose);
+                    fab_income_txt.setClickable(false);
+                    fab_expense_txt.setClickable(false);
+                    isOpen=false;
+                }else {
+                    fab_income_btn.startAnimation(FadeOpen);
+                    fab_expense_btn.startAnimation(FadeOpen);
+                    fab_income_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+
+                    fab_income_txt.startAnimation(FadeOpen);
+                    fab_expense_txt.startAnimation(FadeOpen);
+                    fab_income_txt.setClickable(true);
+                    fab_income_txt.setClickable(true);
+                    isOpen=true;
+                }
+
+            }
+        });
+
+
+        return myviem;
     }
 }
