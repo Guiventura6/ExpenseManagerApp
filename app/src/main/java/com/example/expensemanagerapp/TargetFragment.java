962e0feacc2f2902384d02899a2eb37e34f26c71
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-02 12:38:38 -0300

    Incluindo objetivos criados no recycler view

--- app/src/main/java/com/example/expensemanagerapp/TargetFragment.java
+++ app/src/main/java/com/example/expensemanagerapp/TargetFragment.java
@@ -2,8 +2,11 @@ package com.example.expensemanagerapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -11,9 +14,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.expensemanagerapp.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,6 +37,18 @@ public class TargetFragment extends Fragment {
 
     private FloatingActionButton fab_target_btn;
 
+    //Recycler view
+    private RecyclerView recyclerView;
+    private FirebaseRecyclerAdapter adapter;
+
+    private String post_key;
+
+    // Data item
+    private String title;
+    private String dateTarget;
+    private int amount;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,9 +65,13 @@ public class TargetFragment extends Fragment {
         String uid=mUser.getUid();
 
         mTargetDatabase= FirebaseDatabase.getInstance().getReference().child("TargetData").child(uid);
+        mTargetDatabase.keepSynced(true);
 
         //Connect floating button to layout
         fab_target_btn=myview.findViewById(R.id.fb_target_plus_btn);
+
+        //Recycler
+        recyclerView=myview.findViewById(R.id.recycler_id_target);
 
         fab_target_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +79,13 @@ public class TargetFragment extends Fragment {
                 targetDataInsert();
             }
         });
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
 
 
         return myview;
@@ -122,7 +151,85 @@ public class TargetFragment extends Fragment {
             }
         });
 
-
         dialog.show();
     }
+
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Data> options = new FirebaseRecyclerOptions.Builder<Data>()
+                .setQuery(mTargetDatabase, Data.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Data, TargetFragment.MyViewHolder>(options){
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new TargetFragment.MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.target_recycler_data, parent, false));
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Data model) {
+                // Criar um novo modelo de dados para dados de objetivos.
+                holder.setAmount(model.getAmount());
+                holder.setTarget(model.getType());
+                holder.setDateTarget(model.getNote());
+                holder.setDateInclude(model.getDate());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        int pos=holder.getBindingAdapterPosition();
+                        post_key=getRef(pos).getKey();
+
+                        title=model.getType();
+                        dateTarget=model.getNote();
+                        amount=model.getAmount();
+
+                        // updateDataItem();
+                    }
+                });
+
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    private static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView=itemView;
+        }
+
+        void setTarget(String target) {
+            TextView mType=mView.findViewById(R.id.title_txt_target);
+            mType.setText(target);
+        }
+
+        void setDateTarget(String dateTarget){
+            TextView mNote=mView.findViewById(R.id.date_txt_target);
+            mNote.setText(dateTarget);
+        }
+
+        void setDateInclude(String date_server){
+            TextView mDateInclude=mView.findViewById(R.id.date_server_txt_target);
+            mDateInclude.setText(date_server);
+        }
+
+        void setAmount(int amount){
+            TextView mAmount=mView.findViewById(R.id.amount_txt_target);
+            String strAmount=String.valueOf(amount);
+            mAmount.setText(strAmount);
+        }
+
+    }
+
+
 }
