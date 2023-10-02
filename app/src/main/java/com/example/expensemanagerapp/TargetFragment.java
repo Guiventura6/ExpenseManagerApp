19c71882e5aef3278cb5ddf11bc752a653b0ea2b
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-02 10:05:35 -0300

    Incluindo tela de cadastro ao clicar em incluir objetivo

--- app/src/main/java/com/example/expensemanagerapp/TargetFragment.java
+++ app/src/main/java/com/example/expensemanagerapp/TargetFragment.java
@@ -2,63 +2,78 @@ package com.example.expensemanagerapp;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TargetFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class TargetFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TargetFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TargetFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TargetFragment newInstance(String param1, String param2) {
-        TargetFragment fragment = new TargetFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FloatingActionButton fab_target_btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_target, container, false);
+        View myview=inflater.inflate(R.layout.fragment_target, container, false);
+
+        //Connect floating button to layout
+        fab_target_btn=myview.findViewById(R.id.fb_target_plus_btn);
+
+        fab_target_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                targetDataInsert();
+            }
+        });
+
+
+        return myview;
+    }
+
+    private void targetDataInsert() {
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+        View myview=inflater.inflate(R.layout.insert_target_item,null);
+        mydialog.setView(myview);
+
+        final AlertDialog dialog=mydialog.create();
+        dialog.setCancelable(false);
+
+
+        final EditText edtTarget=myview.findViewById(R.id.target_edt);
+        final EditText edtDate=myview.findViewById(R.id.date_edt);
+        final EditText edtAmount=myview.findViewById(R.id.amount_edt);
+
+        Button btnSave=myview.findViewById(R.id.btn_save);
+        Button btnCancel=myview.findViewById(R.id.btn_cancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // HERE
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
     }
 }
